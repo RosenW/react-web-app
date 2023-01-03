bdd5a35b792c57fb7dcefb1e231a25e1a60fee79
RosenW <onehundredandfourtytwo@gmail.com>
2023-01-03 17:05:22 +0200

    working sidebar buttons, added text and fractal content components. added some content.

---
+++ web-app/src/text_files/1_computer_programming.js
@@ -0,0 +1,19 @@
+const text = `Computer Programming
+    1. Purpose
+        - The purpose of programming is to find a sequence of instructions that will automate the performance of a task on a computer, often for solving a given problem.
+
+    2. Main Concepts
+        - Digital Information/Data
+            - Bits - The bit is the most basic unit of information in computing and digital communications. The bit represents a logical state with one of two possible values. These values are most commonly represented as either "1" or "0".
+            - Bytes - The byte is a unit of digital information that most commonly consists of eight bits (единица за дигитална информация - 8 бита)
+        - Automation
+            - Automation is the creation and application of technologies to produce and deliver goods and services with minimal human intervention
+            - Механизъм позволяващ на програмистите да изнесат писането на код едно ниво по-високо в абстракцията
+        - Main Functionalities
+            - Приемане на Input
+            - Изкарване на Output
+            - Save Data (Запазване на Информация)
+            - Move Data (Местене на Информация)
+`;
+
+export default text;
---
+++ web-app/src/text_files/welcome.js
@@ -0,0 +1,3 @@
+const text = `Hello and welcome to my website, please select a tab from the navigation bar to start exploring.`;
+
+export default text;
